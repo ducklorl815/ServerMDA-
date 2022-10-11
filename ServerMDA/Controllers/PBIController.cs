4a28beb9afb76fe97996907b52dc707aeeb19f03
家毅 廖 <ducklorl815@gmail.com>
2022-10-11 13:21:48 +0800

    更新pbi報表

---
+++ ServerMDA/Controllers/PBIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerMDA.Controllers
+{
+    public class PBIController : Controller
+    {
+        public IActionResult PBI()
+        {
+            return View();
+        }
+    }
+}
