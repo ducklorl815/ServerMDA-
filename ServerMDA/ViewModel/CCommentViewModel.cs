7e3ccfba936c63c3e3b207b65b696289f732bfad
Yuka12948 <yuka12948@gmail.com>
2022-10-31 09:41:01 +0800

    221031 會員管理.評論管理 >> 版型調整 內容修正 除bug 增加demo鍵

--- ServerMDA/ViewModel/CCommentViewModel.cs
+++ ServerMDA/ViewModel/CCommentViewModel.cs
@@ -119,7 +119,7 @@ namespace ServerMDA.ViewModel
         public string 對象名字fName { get; set; }
 
         //public virtual 電影movie 電影編號movie { get; set; }
-        [DisplayName("文章標題")]
+        [DisplayName("電影名稱")]
         public string 中文標題titleCht { get; set; }
         [DisplayName("英文標題")]
         public string 英文標題titleEng { get; set; }
