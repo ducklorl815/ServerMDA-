f433ca4358ea8ce46d6c3680d951709f683fabed
ducklorl815 <75732514+ducklorl815@users.noreply.github.com>
2022-11-01 02:44:39 +0800

    首頁製作 charts

--- ServerMDA/Controllers/HomePageController.cs
+++ ServerMDA/Controllers/HomePageController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServerMDA.Models;
+using ServerMDA.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,17 @@ namespace ServerMDA.Controllers
     {
         public IActionResult List()
         {
-            return View();
+            MDAContext db = new MDAContext();
+            CTheaterViewModel datas = null;
+            var theater = db.電影院theaters.Select(p => p.電影院名稱theaterName).ToList();
+            datas = db.電影院theaters.Select
+            (p => new CTheaterViewModel
+            {
+                theater = p,
+
+            }).FirstOrDefault();
+            datas.titleTheater = theater;
+            return View(datas);
         }
     }
 }
